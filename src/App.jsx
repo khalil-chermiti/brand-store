4526ab3a6a8392f62f067d02b465f975a9f81acd
khalil chermiti <khalil666chermiti@gmail.com>
2022-03-13 22:52:04 +0100

    built form , singin and button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react' 
 import { Routes , Route} from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homePage.component';
 import ShopPage from './pages/shoppage/shop.component';
-import Header from './components/header/header.component';
+import SignINandUpPage from './pages/signingpage/signing.component'; 
+
 import './App.css'
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/shop' element={<ShopPage/>}/>
+        <Route path='/signin' element={<SignINandUpPage/>}/>
       </Routes>
     </div>
   );
---
+++ src/pages/signingpage/signing.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './signing.style.scss';
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignINandUpPage = () => (
+    <div>
+        <SignIn/>
+    </div>
+)
+
+export default SignINandUpPage ;
