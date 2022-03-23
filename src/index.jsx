4835f078e9e0643c9b82d53b4058b331225b4dbf
khalil chermiti <khalil666chermiti@gmail.com>
2022-03-23 14:31:28 +0100

    implemented persistant storage using persist-redux package

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as Redux } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import {store , persistor } from './redux/store';
 
 import App from './App';
 import './index.css';
@@ -10,9 +12,11 @@ import './index.css';
 ReactDOM.render(
   <Redux store={store}>
     <Router>
-      <React.StrictMode>
-        <App/>
-      </React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <App/>
+        </React.StrictMode>
+      </PersistGate>
     </Router> 
   </Redux>,
   document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import {combineReducers} from 'redux' ; 
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-  
-export default combineReducers({
+
+
+// persistor config
+const persitConfig = {
+    key : 'root' ,
+    storage :storage,
+    whitelist : ['cart'] , 
+}
+
+// root reducer ;
+const rootReducer = combineReducers({
     user : userReducer ,
     cart : cartReducer ,
 });
+
+
+// persistReducer return a rootReducer with persistance capabilities
+export default persistReducer(persitConfig , rootReducer) ;
